19ad85bbea5ad29e44f016f2c5f919f02c6d6c74
227214 <227214@KB37-120-C5>
2025-10-17 16:34:57 +0300

    ui

--- TodoListApp.Models/EditTaskWindow.xaml.cs
+++ TodoListApp.Models/EditTaskWindow.xaml.cs
@@ -5,14 +5,27 @@ namespace TodoListApp
 {
     public partial class EditTaskWindow : Window
     {
-        
+
+        public TodoItem Task { get; set; }
 
         public EditTaskWindow(TodoItem task)
         {
             InitializeComponent();
-            
+            Task = task;
+            DataContext = Task;
+            Owner = Application.Current.MainWindow;
         }
 
-       
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
     }
 }
